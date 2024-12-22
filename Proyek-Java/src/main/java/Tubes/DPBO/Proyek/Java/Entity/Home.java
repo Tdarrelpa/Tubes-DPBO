50a8886b0cf87678ad6f864ee5cbb1cf3169a08a
Timotius Darrel Putra Arma <146645317+Tdarrelpa@users.noreply.github.com>
2024-12-23 01:08:11 +0700

    Create Home.java

---
+++ Proyek-Java/src/main/java/Tubes/DPBO/Proyek/Java/Entity/Home.java
@@ -0,0 +1,10 @@
+package Tubes.DPBO.Proyek.Java.Entity;
+
+public class Home
+{
+    public void menu()
+    {
+        //Placeholder
+        System.out.println("Welcome to the home page");
+    }
+}
